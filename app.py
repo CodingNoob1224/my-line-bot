ca6d509e52018ff8895290e4b2d575150a54c66b
CodingNoob <CodingNoob1224@gmail.com>
2025-03-18 15:10:43 +0800

    test2

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def callback():
 def handle_message(event):
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info( ReplyMessageRequest( reply_token=event.reply_token, messages="測試"))
+        line_bot_api.reply_message_with_http_info( ReplyMessageRequest( reply_token=event.reply_token, messages=[TextMessage(text="測試")]))
 
         # line_bot_api.reply_message_with_http_info( ReplyMessageRequest( reply_token=event.reply_token, messages=[TextMessage(text=event.message.text)]))
 
